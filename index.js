897078387c900159cade847432b83ecb4a9ffedd
James Parrish <jthomasparrishjr@gmail.com>
2022-06-01 20:09:09 -0500

    commit tree 892f09aad52694f93a48d615ab93600d4a9ece89

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution here
+function findMatching(drivers, string){
+    return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase())
+}
+function fuzzyMatch(drivers, letters){
+    return drivers.filter(index => index.startsWith(letters))
+  }
+function matchName(drivers, string){
+    return drivers.filter(i => i.name === string)
+}
